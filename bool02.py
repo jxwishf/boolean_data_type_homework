49c48051f30ae8013c3ee6da12017fe822450b5e
ja <jaxa42421@gmail.com>
2025-06-05 16:19:09 +0500

    task2

--- bool02.py
+++ bool02.py
@@ -1,10 +1,7 @@
 def main(a):
-    """
-    check the following statement "The variable 'a' is equal 7"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    if a == 7:
+       return True
+    else: 
+        return False
+
+print(main(7))

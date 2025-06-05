e414e33703782cc42a7919cb2ade04ec356c1cfa
ja <jaxa42421@gmail.com>
2025-06-05 16:23:11 +0500

    task3

--- bool03.py
+++ bool03.py
@@ -1,10 +1,8 @@
 def main(b):
-    """
-    check the following statement "The variable "b" is positive"
-    Args:
-        b: int
-    Returns:
-        bool
-    """
-    # Write your code here
+    if b<0:
+        return False
+    else:
+        return True
     return 
+
+print(main(-1))

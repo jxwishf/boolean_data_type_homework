f48de84dc3453b71c6a5489cdb7f98eee94e7f41
ja <jaxa42421@gmail.com>
2025-06-05 16:27:14 +0500

    task5

--- bool05.py
+++ bool05.py
@@ -1,10 +1,7 @@
 def main(a):
-    """
-    check the following statement "The variable "a" is an odd number"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
+    if a%2 == 0:
+        return True
+    else:
+        return False
     return 
+print(main(5))

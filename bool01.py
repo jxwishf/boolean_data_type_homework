3bf6f160c639820548f403294d7e2aeb9dcb3285
ja <jaxa42421@gmail.com>
2025-06-05 16:13:03 +0500

    task1

--- bool01.py
+++ bool01.py
@@ -1,11 +1,3 @@
 def main(a,b):
-    """
-    Check the following statement "they are equal"
-    Args:
-        a: int
-        b: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return bool(a and b)
+print(main(1,1))
